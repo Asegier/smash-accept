3a58d0d65d7d3f5b5f9db8287624a082d5520fed
CJ Chan <cjchan@CJs-MacBook-Air.local>
2025-06-07 15:44:47 -0700

    fix port issue

--- main.py
+++ main.py
@@ -98,4 +98,5 @@ def home():
         return "<h2>No new 'Accept' links found in the emails.</h2>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
